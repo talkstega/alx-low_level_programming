c79894606db907caf641f6329c055a611eeb742d
obeya sunday udeh <talkstega@yahoo.com>
2022-07-18 11:13:43 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,27 @@
-checker
+#include "main.h"
+
+/**
+ * _strstr -  function that  locates a substring.
+ * @haystack: pointer to the character.
+ * @needle: character locator to consider.
+ * Return: 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
