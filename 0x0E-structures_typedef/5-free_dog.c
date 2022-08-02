386ec6796258e27ab46c3ae2b8b0f92f5d360813
obeya sunday udeh <talkstega@yahoo.com>
2022-08-02 15:25:36 +0100

    Update

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,15 @@
-checker
+#include "dog.h"
 
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
