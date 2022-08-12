ac794b48962f15888fe041cb2794fbb226b1b92f
obeya sunday udeh <talkstega@yahoo.com>
2022-08-12 17:47:10 +0100

    Update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,13 @@
-checker
+#include <stdio.h>
+
+void theHare(void)__attribute__((constructor));
+
+/**
+ * theHare - prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed.
+ */
+void theHare(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
