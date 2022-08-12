4bc6fff9fe60766792419c8b7949f982a95bb1b4
obeya sunday udeh <talkstega@yahoo.com>
2022-08-12 17:56:38 +0100

    Update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,50 +1,20 @@
 #include "lists.h"
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
 
-	while (*s)
-		s++;
-	return (s - p);
-}
 /**
- * add_node_end - adds a new node at the end of a list_t list;
- * @head: reference to head of list
- * @str: string to be added to linked list
- * Return: address of new node
+ * free_list - frees a list_t list
+ * @head: head of list_t
  */
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-	list_t *newNode;
-	list_t *last = *head;
-	char *newStr;
+	list_t *current = head;
+	list_t *next;
 
-	if (!str)
-		return (NULL);
-	newNode = malloc(sizeof(list_t));
-	if (!newNode)
-		return (NULL);
-	newStr = strdup(str);
-	if (!newStr)
+	while (current)
 	{
-		free(newNode);
-		return (NULL);
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
 	}
-	newNode->len = _strlen(newStr);
-	newNode->str = newStr;
-	newNode->next = NULL;
-	if (!*head)
-	{
-		*head = newNode;
-		return (newNode);
-	}
-	while (last->next)
-		last = last->next;
-	last->next = newNode;
-	return (last);
+	head = NULL;
 }
